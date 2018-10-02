7e47df60648325aa19523ee31bf59918aa8b9caf
kevin <awed3310@gmail.com>
2018-10-02 16:35:33 +0800

    sec

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -2,6 +2,11 @@ package com.tom.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bmi(View view) {
+        EditText edweight = findViewById(R.id.ed_weight);
+        EditText edheight = findViewById(R.id.ed_height);
+        Log.d("MainActivity", "bmi");
+        String w = edweight. getText().toString();
+        String h = edheight. getText().toString();
+        Log.d("MainActivity" ,w+"/"+h);
+        float weight =Float.parseFloat(w);
+        float height =Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity", bmi+"" );
     }
 }
