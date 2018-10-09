787543cce5b202d26dc2d0f891e98fe7757e1509
Gangster69 <awed3310@gmail.com>
2018-10-09 16:22:42 +0800

    20181009

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,24 +1,31 @@
 package com.tom.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edweight;
+    private EditText edheight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edweight = findViewById(R.id.ed_weight);
+        edheight = findViewById(R.id.ed_height);
     }
 
     public void bmi(View view) {
-        EditText edweight = findViewById(R.id.ed_weight);
-        EditText edheight = findViewById(R.id.ed_height);
+
         Log.d("MainActivity", "bmi");
         String w = edweight. getText().toString();
         String h = edheight. getText().toString();
@@ -27,5 +34,19 @@ public class MainActivity extends AppCompatActivity {
         float height =Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity", bmi+"" );
+        Toast.makeText(this, "your bmi is "+bmi, Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage("your bmi is "+bmi)
+                .setTitle("BMI")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edweight.setText("");
+                        edheight.setText("");
+                    }
+
+                })
+                .show();
+
     }
 }
